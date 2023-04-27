e3e2a725b30a209bb5fe152a2cc4ef970caa3ae2
hautran_itr_intern <hauken146@gmail.com>
2023-04-27 20:03:19 +0700

    update : add layout_login_prj

---
+++ trn_s_application-v1.0.1/app/src/test/java/com/trnsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.trnsapplication.app
+
+import com.trnsapplication.app.appcomponents.base.BaseActivity
+import com.trnsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
